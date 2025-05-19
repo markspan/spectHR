792eb3245161f0b6a15f734a8bae9b7cd988bc98
Mark M. Span <m.m.span@rug.nl>
2025-05-19 22:22:09 +0200

    Update mainwindow.py

--- mainwindow.py
+++ mainwindow.py
@@ -158,9 +158,8 @@ class MainWindow(QMainWindow):
             Clean up the raw ECG signal: 
             """
             QApplication.setOverrideCursor(Qt.WaitCursor)
-            cleanSet = cs.ecgArtifactDetection(self.dataset, par = {'dtw_thresh': 100000})
+            self.dataset = cs.ecgArtifactDetection(self.dataset, par = {'dtw_thresh': 100000})
             QApplication.restoreOverrideCursor()
-            self.dataset.ecg = cleanSet
             self.dataset = cs.calcPeaks(self.dataset)
             self.show_preprocessing_plot(self.dataset)
 
